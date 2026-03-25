912f59c3b279fae2b0418992af8281ae7cd03142
Syed Jubair <m.syedjubair@gmail.com>
2026-03-25 13:16:30 +0600

    Added max duration in inngest

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,6 +2,9 @@ import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { helloWorld, generateSummary, editCode } from "@/inngest/functions";
 
+// This endpoint can run for a maximum of 300 seconds
+export const maxDuration = 300;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
