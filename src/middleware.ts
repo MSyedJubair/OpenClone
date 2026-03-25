6ff080537e0f6eb58721e752320f99020ac93051
Syed Jubair <m.syedjubair@gmail.com>
2026-03-25 12:22:26 +0600

    Updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,35 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 
-export function middleware(req: NextRequest) {
-  const isLoggedIn = Boolean(req.cookies.get("better-auth.session_token"));
+export async function middleware(req: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-  const protectedRoutes = ["/dashboard", "/project", "/settings"];
-
-  const isProtected = protectedRoutes.some((route) =>
-    req.nextUrl.pathname.startsWith(route),
-  );
-
-  if (isProtected && !isLoggedIn) {
+  if (!session) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
+  // const isLoggedIn =
+  //   Boolean(req.cookies.get("better-auth.session_token")) ||
+  //   Boolean(req.cookies.get("__Secure-better-auth.session_token"));
+
+  // const protectedRoutes = ["/dashboard", "/project", "/settings"];
+
+  // const isProtected = protectedRoutes.some((route) =>
+  //   req.nextUrl.pathname.startsWith(route),
+  // );
+
+  // if (isProtected && !isLoggedIn) {
+  //   return NextResponse.redirect(new URL("/sign-in", req.url));
+  // }
+
   return NextResponse.next();
 }
+export const runtime = "nodejs"; 
 
 export const config = {
-  matcher: ["/project/:path*", "/dashboard", "/settings"],
+  matcher: ["/project/:path*", "/dashboard", "/settings", "/profile"],
 };
